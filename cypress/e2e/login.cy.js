c938f75325fcfaa533a5857c24280e511e917082
João Eduardo Cardoso Padilha <duducardoso2701@gmail.com>
2026-05-13 20:33:08 -0300

    Alterações no teste de usuário

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -9,18 +9,34 @@ describe('Login', () => {
     cy.url().should('include', '/inventory.html')
   })
 
-  it('Retornar erro com usuário bloqueado', () => {
+  it('Retornar erro ao logar com usuário inexistente', () => {
     cy.visit('https://www.saucedemo.com/')
 
-    cy.get('[data-test="username"]').type('locked_out_user')
-    cy.get('[data-test="password"]').type('secret_sauce')
+    cy.get('[data-test="username"]').type('user_invalid')
+    cy.get('[data-test="password"]').type('12345678')
     cy.get('[data-test="login-button"]').click()
 
 
     cy.get('[data-test="error"]')
       .should(
         'contain.text', 
-        'Epic sadface: Sorry, this user has been locked out.'
+        'Username and password do not match any user in this service'
+      )
+
+    cy.url().should('eq', 'https://www.saucedemo.com/')
+  })
+
+  it('Retornar erro ao logar com senha incorreta', () => {
+    cy.visit('https://www.saucedemo.com/')
+
+    cy.get('[data-test="username"]').type('standard_user')
+    cy.get('[data-test="password"]').type('wrong_password')
+    cy.get('[data-test="login-button"]').click()
+
+    cy.get('[data-test="error"]')
+      .should(
+        'contain.text', 
+        'Username and password do not match any user in this service'
       )
 
     cy.url().should('eq', 'https://www.saucedemo.com/')
